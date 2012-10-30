c0999ac5bbd17e502cb2676e5b0ac8dc4f13e46a
Ross Boucher <rboucher@gmail.com>
2012-10-29 22:13:07 -0700

    Remove the conflicting definition of transactions for transfers.

--- lib/stripe/transfer.rb
+++ lib/stripe/transfer.rb
@@ -1,16 +1,5 @@
 module Stripe
   class Transfer < APIResource
     include Stripe::APIOperations::List
-
-    def transactions(params={})
-      response, api_key = Stripe.request(:get, transactions_url, @api_key, params)
-      Util.convert_to_stripe_object(response, api_key)
-    end
-
-    private
-
-    def transactions_url
-      url + '/transactions'
-    end
   end
 end

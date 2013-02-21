b2aafa854e49d1c8b935afdf159ff92b490243bd
Ross Boucher <rboucher@gmail.com>
2013-02-21 11:54:16 -0800

    Add id to the list of permanent attributes

--- lib/stripe/stripe_object.rb
+++ lib/stripe/stripe_object.rb
@@ -3,7 +3,7 @@ module Stripe
     include Enumerable
 
     attr_accessor :api_key
-    @@permanent_attributes = Set.new([:api_key])
+    @@permanent_attributes = Set.new([:api_key, :id])
 
     # The default :id method is deprecated and isn't useful to us
     if method_defined?(:id)

d291020a43ad24adcec7bc7efa076550e797cdb9
Andrew Metcalf <andrew@stripe.com>
2013-08-06 15:47:33 -0700

    clean up syntax

--- lib/stripe/stripe_object.rb
+++ lib/stripe/stripe_object.rb
@@ -126,12 +126,10 @@ module Stripe
           define_method(k) { @values[k] }
           define_method(k_eq) do |v|
             if v == ""
-              raise ArgumentError.new(<<EOF.gsub(/\n/, ' ')
-You cannot set #{k} to an empty string.
-We interpret empty strings as nil in requests.
-You may set #{self}.#{k} = nil to delete the property.
-EOF
-                )
+              raise ArgumentError.new(
+                "You cannot set #{k} to an empty string." +
+                "We interpret empty strings as nil in requests." +
+                "You may set #{self}.#{k} = nil to delete the property.")
             end
             @values[k] = v
             @unsaved_values.add(k)

57656f6e8938d7b4677a226b94b4965068e9a8a0
kiran-b <kiran@stripe.com>
2013-10-07 17:41:46 -0700

    update now accounts for metadata={'key' => 'val'} syntax

--- lib/stripe/api_operations/update.rb
+++ lib/stripe/api_operations/update.rb
@@ -3,7 +3,12 @@ module Stripe
     module Update
       def save
         if @unsaved_values.length > 0
-          values = @unsaved_values.reduce({}) do |h, k| 
+          values = @unsaved_values.reduce({}) do |h, k|
+            # if we're updating (but not unsetting) metadata, unset existing keys
+            if k==:metadata and not @values[:metadata].nil?
+              @values[:metadata].update(metadata_unset_hash(@values[:metadata]))
+            end
+
             h.update(k => @values[k].nil? ? '' : @values[k])
           end
           values.delete(:id)
@@ -11,6 +16,15 @@ module Stripe
           refresh_from(response, api_key)
         end
         self
+      end
+
+      def metadata_unset_hash(updated_hash)
+        unset = {}
+        @previous_metadata.each do |key, val|
+          # don't unset if setting to a new value in the same request
+          unset.update(key => '') if not updated_hash.has_key?(key.to_s)
+        end
+        unset
       end
     end
   end

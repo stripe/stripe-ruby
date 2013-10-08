b02b557b74c4664a99b6ae63d45008826f9024fd
Brian Collins <brian@stripe.com>
2013-10-08 17:08:58 +0100

    Style tweaks

--- lib/stripe/api_operations/update.rb
+++ lib/stripe/api_operations/update.rb
@@ -5,7 +5,7 @@ module Stripe
         if @unsaved_values.length > 0
           values = @unsaved_values.reduce({}) do |h, k|
             # if we're updating (but not unsetting) metadata, unset existing keys
-            if k==:metadata and not @values[:metadata].nil?
+            if k == :metadata && @values[:metadata] != nil
               @values[:metadata].update(metadata_unset_hash(@values[:metadata]))
             end
 
@@ -22,7 +22,7 @@ module Stripe
         unset = {}
         @previous_metadata.each do |key, val|
           # don't unset if setting to a new value in the same request
-          unset.update(key => '') if not updated_hash.has_key?(key.to_s)
+          unset.update(key => '') unless updated_hash.has_key?(key.to_s)
         end
         unset
       end

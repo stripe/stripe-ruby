8b3a00c58740f48d1c5ae66fd435960ff6eea49a
Andrew Thorp <andrewpthorp@gmail.com>
2014-01-24 21:27:36 -0800

    cleaning up a pull request from @thomasbrus

--- lib/stripe/stripe_object.rb
+++ lib/stripe/stripe_object.rb
@@ -102,16 +102,6 @@ module Stripe
       @values.each(&blk)
     end
 
-    if RUBY_VERSION < '1.9.2'
-      def respond_to?(symbol)
-        @values.has_key(symbol) || super
-      end
-    else
-      def respond_to_missing?(symbol, include_private = false)
-        @values.has_key?(symbol) || super
-      end
-    end
-
     protected
 
     def metaclass
@@ -172,6 +162,16 @@ module Stripe
         else
           raise
         end
+      end
+    end
+
+    def respond_to_missing?(symbol, include_private = false)
+      @values.has_key?(symbol) || super
+    end
+
+    if RUBY_VERSION < '1.9.2'
+      def respond_to?(symbol)
+        @values.has_key?(symbol) || super
       end
     end
   end

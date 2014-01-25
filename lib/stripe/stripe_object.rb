8b5f7d038ccfffd839d8a37f16cb753a00a76a73
Andrew Thorp <andrewpthorp@gmail.com>
2014-01-24 23:37:09 -0800

    whoops, respond_to? cannot be protected

--- lib/stripe/stripe_object.rb
+++ lib/stripe/stripe_object.rb
@@ -102,6 +102,12 @@ module Stripe
       @values.each(&blk)
     end
 
+    if RUBY_VERSION < '1.9.2'
+      def respond_to?(symbol)
+        @values.has_key?(symbol) || super
+      end
+    end
+
     protected
 
     def metaclass
@@ -167,12 +173,6 @@ module Stripe
 
     def respond_to_missing?(symbol, include_private = false)
       @values.has_key?(symbol) || super
-    end
-
-    if RUBY_VERSION < '1.9.2'
-      def respond_to?(symbol)
-        @values.has_key?(symbol) || super
-      end
     end
   end
 end

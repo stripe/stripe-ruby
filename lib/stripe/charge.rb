f26dc69c85490e693971f6c12aa4c71668921179
Andrew Thorp <andrewpthorp@gmail.com>
2014-01-25 15:08:15 -0800

    per-object credentials on charges

--- lib/stripe/charge.rb
+++ lib/stripe/charge.rb
@@ -4,27 +4,31 @@ module Stripe
     include Stripe::APIOperations::Create
     include Stripe::APIOperations::Update
 
-    def refund(params={})
-      response, api_key = Stripe.request(:post, refund_url, @api_key, params)
-      refresh_from(response, api_key)
+    def refund(params={}, api_key=nil)
+      api_key ||= @api_key
+      response, key = Stripe.request(:post, refund_url, api_key, params)
+      refresh_from(response, key)
       self
     end
 
-    def capture(params={})
-      response, api_key = Stripe.request(:post, capture_url, @api_key, params)
-      refresh_from(response, api_key)
+    def capture(params={}, api_key=nil)
+      api_key ||= @api_key
+      response, key = Stripe.request(:post, capture_url, api_key, params)
+      refresh_from(response, key)
       self
     end
 
-    def update_dispute(params)
-      response, api_key = Stripe.request(:post, dispute_url, @api_key, params)
-      refresh_from({ :dispute => response }, api_key, true)
+    def update_dispute(params, api_key=nil)
+      api_key ||= @api_key
+      response, key = Stripe.request(:post, dispute_url, api_key, params)
+      refresh_from({ :dispute => response }, key, true)
       dispute
     end
 
-    def close_dispute
-      response, api_key = Stripe.request(:post, close_dispute_url, @api_key)
-      refresh_from(response, api_key)
+    def close_dispute(api_key=nil)
+      api_key ||= @api_key
+      response, key = Stripe.request(:post, close_dispute_url, api_key)
+      refresh_from(response, key)
       self
     end
 

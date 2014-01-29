8231fba217945054b5336fb95a6774c514aef309
Jim Danz <jim@stripe.com>
2014-01-29 14:57:40 -0800

    Revert "Clearing out subscription code/tests from Customer."
    This reverts commit d6ebab33109ff2501ba9709d2ac1dd666be0ab67.
    We'll support the legacy single-subscription API style
    indefinitely, and also we generaly try to make it so that
    people can use modern bindings with out-of-date API versions.

--- test/stripe/customer_test.rb
+++ test/stripe/customer_test.rb
@@ -32,6 +32,32 @@ module Stripe
       assert_equal "c_test_customer", c.id
     end
 
+    should "be able to update a customer's subscription" do
+      @mock.expects(:get).once.returns(test_response(test_customer))
+      c = Stripe::Customer.retrieve("test_customer")
+
+      @mock.expects(:post).once.with do |url, api_key, params|
+        url == "#{Stripe.api_base}/v1/customers/c_test_customer/subscription" && api_key.nil? && CGI.parse(params) == {'plan' => ['silver']}
+      end.returns(test_response(test_subscription('silver')))
+      s = c.update_subscription({:plan => 'silver'})
+
+      assert_equal 'subscription', s.object
+      assert_equal 'silver', s.plan.identifier
+    end
+
+    should "be able to cancel a customer's subscription" do
+      @mock.expects(:get).once.returns(test_response(test_customer))
+      c = Stripe::Customer.retrieve("test_customer")
+
+      # Not an accurate response, but whatever
+
+      @mock.expects(:delete).once.with("#{Stripe.api_base}/v1/customers/c_test_customer/subscription?at_period_end=true", nil, nil).returns(test_response(test_subscription('silver')))
+      c.cancel_subscription({:at_period_end => 'true'})
+
+      @mock.expects(:delete).once.with("#{Stripe.api_base}/v1/customers/c_test_customer/subscription", nil, nil).returns(test_response(test_subscription('silver')))
+      c.cancel_subscription
+    end
+
     should "be able to delete a customer's discount" do
       @mock.expects(:get).once.returns(test_response(test_customer))
       c = Stripe::Customer.retrieve("test_customer")

974f5ce42595ada096671bb5deeffbf4276250e0
Matt Colyer <mcolyer@github.com>
2014-02-24 14:16:21 -0800

    Check for @values in StripeObject#method_not_found
    If a StripeObject is being deserialized by psych, @values.has_key? is
    called before @values is initialized which prevents proper
    deserialization from occurring. Checking for existence first resolves
    the issue.

--- lib/stripe/stripe_object.rb
+++ lib/stripe/stripe_object.rb
@@ -181,7 +181,7 @@ module Stripe
     end
 
     def respond_to_missing?(symbol, include_private = false)
-      @values.has_key?(symbol) || super
+      @values && @values.has_key?(symbol) || super
     end
   end
 end

c25d4b0cbf5446ec3e58c575162556e0777da59a
Stephen Wan <stephen@stripe.com>
2014-05-27 16:59:22 -0700

    Cleaned up transfer tests

--- test/stripe/transfer_test.rb
+++ test/stripe/transfer_test.rb
@@ -4,22 +4,22 @@ module Stripe
   class TransferTest < Test::Unit::TestCase
     should "retrieve should retrieve transfer" do
       @mock.expects(:get).once.returns(test_response(test_transfer))
-      i = Stripe::Transfer.retrieve('tr_test_transfer')
-      assert_equal 'tr_test_transfer', i.id
+      transfer = Stripe::Transfer.retrieve('tr_test_transfer')
+      assert_equal 'tr_test_transfer', transfer.id
     end
 
     should "create should create a transfer" do
       @mock.expects(:post).once.returns(test_response(test_transfer))
-      i = Stripe::Transfer.create
-      assert_equal "tr_test_transfer", i.id
+      transfer = Stripe::Transfer.create
+      assert_equal "tr_test_transfer", transfer.id
     end
 
     should "cancel should cancel a transfer" do
       @mock.expects(:get).once.returns(test_response(test_transfer))
-      i = Stripe::Transfer.retrieve('tr_test_transfer')
+      transfer = Stripe::Transfer.retrieve('tr_test_transfer')
 
       @mock.expects(:post).once.with('https://api.stripe.com/v1/transfers/tr_test_transfer/cancel', nil, '').returns(test_response(test_canceled_transfer))
-      s = i.cancel
+      transfer.cancel
     end
   end
 end

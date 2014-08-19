a5961385269f842745d22575028e6f2249f446f9
Greg Sabo <gregsabo@stripe.com>
2014-08-19 16:24:44 -0700

    Make coupons update-able.

--- lib/stripe/coupon.rb
+++ lib/stripe/coupon.rb
@@ -1,6 +1,7 @@
 module Stripe
   class Coupon < APIResource
     include Stripe::APIOperations::Create
+    include Stripe::APIOperations::Update
     include Stripe::APIOperations::Delete
     include Stripe::APIOperations::List
   end

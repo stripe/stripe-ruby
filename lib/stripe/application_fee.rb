4bef41192a25613f9080fecb3530a4fc7206ec1d
Shale Craig <shale@stripe.com>
2014-12-16 15:43:16 -0800

    Add the ability to use an api_key for application_fee.refund

--- lib/stripe/application_fee.rb
+++ lib/stripe/application_fee.rb
@@ -6,8 +6,8 @@ module Stripe
       '/v1/application_fees'
     end
 
-    def refund(params={})
-      response, api_key = Stripe.request(:post, refund_url, @api_key, params)
+    def refund(params={}, api_key=nil)
+      response, api_key = Stripe.request(:post, refund_url, api_key, params)
       refresh_from(response, api_key)
     end
 

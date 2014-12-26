8f93cd5fac98cbdf054507549d4eca12eb887d6c
Andrew Narkewicz <anark6@gmail.com>
2014-12-26 09:10:23 -0700

    Added deauthorize method to account for deauthorizing from stripe connect application

--- lib/stripe/account.rb
+++ lib/stripe/account.rb
@@ -1,4 +1,9 @@
 module Stripe
   class Account < SingletonAPIResource
+    def deauthorize(client_id, opts={})
+      api_key, headers = Util.parse_opts(opts)
+      response, api_key = Stripe.request(:post, '/oauth/deauthorize', api_key, { 'client_id' => client_id, 'stripe_user_id' => self.id }, headers, Stripe.connect_base)
+      Util.convert_to_stripe_object(response, api_key)
+    end
   end
 end
--- test/stripe/account_test.rb
+++ test/stripe/account_test.rb
@@ -10,5 +10,17 @@ module Stripe
       assert !a.charge_enabled
       assert !a.details_submitted
     end
+
+    should "be able to deauthorize an account" do
+      resp = {:id => 'acct_1234', :email => "test+bindings@stripe.com", :charge_enabled => false, :details_submitted => false}
+      @mock.expects(:get).once.returns(test_response(resp))
+      a = Stripe::Account.retrieve
+
+
+      @mock.expects(:post).once.with do |url, api_key, params|
+        url == "#{Stripe.connect_base}/oauth/deauthorize" && api_key.nil? && CGI.parse(params) == { 'client_id' => [ 'ca_1234' ], 'stripe_user_id' => [ a.id ]}
+      end.returns(test_response({ 'stripe_user_id' => a.id }))
+      a.deauthorize('ca_1234', 'sk_test_1234')
+    end
   end
 end

43d591a93e49a612652444c2fd10fa11dcd474d4
Kyle Conroy <kjc@stripe.com>
2015-01-08 10:32:57 -0800

    Pass along API key

--- lib/stripe/api_operations/delete.rb
+++ lib/stripe/api_operations/delete.rb
@@ -3,7 +3,7 @@ module Stripe
     module Delete
       def delete(params = {}, opts={})
         api_key, headers = Util.parse_opts(opts)
-        response, api_key = Stripe.request(:delete, url, api_key, params, headers)
+        response, api_key = Stripe.request(:delete, url, api_key || @api_key, params, headers)
         refresh_from(response, api_key)
       end
     end

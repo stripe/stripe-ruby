be8108143bff2f7ec0407718526aa8e23c1991ad
Brandur <brandur@mutelight.org>
2015-09-23 12:04:12 -0700

    Assert on entire test URL for a slightly better guarantee

--- test/stripe/bitcoin_transaction_test.rb
+++ test/stripe/bitcoin_transaction_test.rb
@@ -2,14 +2,22 @@ require File.expand_path('../../test_helper', __FILE__)
 
 module Stripe
   class BitcoinTransactionTest < Test::Unit::TestCase
+    TEST_ID = "btctxn_test_transaction".freeze
+
     should "retrieve should retrieve bitcoin receiver" do
-      @mock.expects(:get).once.returns(make_response(make_bitcoin_transaction))
-      receiver = Stripe::BitcoinTransaction.retrieve('bttxn_test_transaction')
-      assert_equal 'btctxn_test_transaction', receiver.id
+      @mock.expects(:get).
+        with("#{Stripe.api_base}/v1/bitcoin/transactions/#{TEST_ID}", nil, nil).
+        once.
+        returns(make_response(make_bitcoin_transaction))
+      receiver = Stripe::BitcoinTransaction.retrieve(TEST_ID)
+      assert_equal TEST_ID, receiver.id
     end
 
     should "all should list bitcoin transactions" do
-      @mock.expects(:get).once.returns(make_response(make_bitcoin_transaction_array))
+      @mock.expects(:get).
+        with("#{Stripe.api_base}/v1/bitcoin/transactions", nil, nil).
+        once.
+        returns(make_response(make_bitcoin_transaction_array))
       transactions = Stripe::BitcoinTransaction.all
       assert_equal 3, transactions.data.length
       assert transactions.data.kind_of? Array

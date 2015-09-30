ac7d050ecf4d433749cb6933e2f7908833d0a65b
Brandur <brandur@stripe.com>
2015-09-29 17:36:01 -0700

    Create if #save is called on a new object
    Modifies the behavior of an update so that it can create an object if it
    was new. This allows an "upsert"-like functionality where the SDK will
    tend to do the right/expected thing.
    
    This is a tad janky in that it checks for the existence of a class-level
    `.create` method which is expected to come from a different module, but
    most modules in the project are already fairly deeply intertwined and
    co-dependent.
    
    Fixes #165.

--- lib/stripe/api_operations/update.rb
+++ lib/stripe/api_operations/update.rb
@@ -3,16 +3,34 @@ module Stripe
     module Update
       def save(params={})
         # Let the caller override the URL but avoid serializing it.
-        req_url = params.delete(:req_url) || url
+        req_url = params.delete(:req_url) || save_url
         values = self.class.serialize_params(self).merge(params)
 
         if values.length > 0
+          # note that id gets removed here our call to #url above has already
+          # generated a uri for this object with an identifier baked in
           values.delete(:id)
 
           response, opts = request(:post, req_url, values)
           refresh_from(response, opts)
         end
         self
+      end
+
+      private
+
+      def save_url
+        # This switch essentially allows us "upsert"-like functionality. If the
+        # API resource doesn't have an ID set (suggesting that it's new) and
+        # its class responds to .create (which comes from
+        # Stripe::APIOperations::Create), then use the URL to create a new
+        # resource. Otherwise, generate a URL based on the object's identifier
+        # for a normal update.
+        if self[:id] == nil && self.class.respond_to?(:create)
+          self.class.url
+        else
+          url
+        end
       end
     end
   end

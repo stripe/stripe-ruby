8c7a976ffba898313408a2fcf272f9ca45616c68
Brandur <brandur@stripe.com>
2015-09-30 11:28:05 -0700

    Space this method out a little bit

--- lib/stripe/stripe_object.rb
+++ lib/stripe/stripe_object.rb
@@ -37,8 +37,10 @@ module Stripe
     def refresh_from(values, opts, partial=false)
       @opts = opts
       @original_values = Marshal.load(Marshal.dump(values)) # deep copy
+
       removed = partial ? Set.new : Set.new(@values.keys - values.keys)
       added = Set.new(values.keys - @values.keys)
+
       # Wipe old state before setting new.  This is useful for e.g. updating a
       # customer, where there is no persistent card parameter.  Mark those values
       # which don't persist as transient
@@ -47,11 +49,13 @@ module Stripe
         remove_accessors(removed)
         add_accessors(added)
       end
+
       removed.each do |k|
         @values.delete(k)
         @transient_values.add(k)
         @unsaved_values.delete(k)
       end
+
       values.each do |k, v|
         @values[k] = Util.convert_to_stripe_object(v, @opts)
         @transient_values.delete(k)

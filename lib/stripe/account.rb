c796958516fcf6b64d948966e73fb4002d50d69a
Brandur <brandur@mutelight.org>
2016-08-30 11:52:16 -0700

    Generalize saving nested resources
    Since #433, saving API resources nested under other API resources has
    not been the default. Instead, any instances where this should occur
    have been special cased with specific method implementations that would
    set the `#save_with_parent` flag when a field is written.
    
    This ended up causing some problems because as seen in #457, because
    places that we need to do this aren't well vetted, some were forgotten.
    
    This makes implementation of new fields that need this behavior simpler
    by implementing a `.save_nested_resource` metraprogramming method on the
    `APIResource` class. This can be called as necessary by any concrete API
    resource implementations.
    
    We replace existing implementatinos and also add one to `Subscription`,
    which had previously been suffering from a similar problem where its
    `#source` had not received a special case.

--- lib/stripe/account.rb
+++ lib/stripe/account.rb
@@ -5,6 +5,12 @@ module Stripe
     include Stripe::APIOperations::Delete
     include Stripe::APIOperations::Save
 
+    save_nested_resource :external_account
+
+    save_nested_resource :bank_account
+    extend Gem::Deprecate
+    deprecate :bank_account=, "#external_account=", 2017, 8
+
     def resource_url
       if self['id']
         super
@@ -26,23 +32,6 @@ module Stripe
         id = nil
       end
       super(id, opts)
-    end
-
-    # Set or replace an account's external account.
-    def external_account=(value)
-      super
-
-      # The parent setter will perform certain useful operations like
-      # converting to an APIResource if appropriate.
-      value = self.external_account
-
-      # Note that external_account may be a card, but could also be a tokenized
-      # card's ID (which is a string), and so we check its type here.
-      if value.is_a?(APIResource)
-        value.save_with_parent = true
-      end
-
-      value
     end
 
     def reject(params={}, opts={})
@@ -109,22 +98,6 @@ module Stripe
     end
 
     ARGUMENT_NOT_PROVIDED = Object.new
-
-    # Set or replace an account's bank account.
-    #
-    # This method is deprecated. Please use #external_account instead.
-    def bank_account=(value)
-      super
-      value = self.bank_account
-
-      if value.is_a?(APIResource)
-        value.save_with_parent = true
-      end
-
-      value
-    end
-    extend Gem::Deprecate
-    deprecate :bank_account=, "#external_account=", 2017, 8
 
     private
 
--- lib/stripe/subscription.rb
+++ lib/stripe/subscription.rb
@@ -5,6 +5,8 @@ module Stripe
     include Stripe::APIOperations::Save
     include Stripe::APIOperations::Delete
 
+    save_nested_resource :source
+
     def delete_discount
       _, opts = request(:delete, discount_url)
       initialize_from({ :discount => nil }, opts, true)

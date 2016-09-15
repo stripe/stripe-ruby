e2cc264c12a289f8c13f817d0b080b9676bdb59b
Vijay Singh <vijay@stripe.com>
2016-09-15 10:14:42 -0700

    Hoist extend and include directives to top of class

--- lib/stripe/apple_pay_domain.rb
+++ lib/stripe/apple_pay_domain.rb
@@ -1,12 +1,12 @@
 module Stripe
   # Domains registered for Apple Pay on the Web
   class ApplePayDomain < APIResource
-    def self.resource_url
-      '/v1/apple_pay/domains'
-    end
-
     extend Stripe::APIOperations::Create
     include Stripe::APIOperations::Delete
     extend Stripe::APIOperations::List
+
+    def self.resource_url
+      '/v1/apple_pay/domains'
+    end
   end
 end

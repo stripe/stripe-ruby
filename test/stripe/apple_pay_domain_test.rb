760e0c0b8839e69f6e768500acff8783de81bef1
Brandur <brandur@mutelight.org>
2016-10-14 16:28:57 -0700

    Modernize Apple Pay Domain and Balance test suites

--- test/stripe/apple_pay_domain_test.rb
+++ test/stripe/apple_pay_domain_test.rb
@@ -2,33 +2,28 @@ require File.expand_path('../../test_helper', __FILE__)
 
 module Stripe
   class ApplePayDomainTest < Test::Unit::TestCase
-    should "create should return a new Apple Pay domain" do
-      @mock.expects(:post).once
-           .with('https://api.stripe.com/v1/apple_pay/domains', nil, '')
-           .returns(make_response(make_apple_pay_domain))
-      d = Stripe::ApplePayDomain.create
-      assert_equal "apwc_test_domain", d.id
-    end
+    include WithoutLegacyStubs
 
-    should "domains should be deletable" do
-      @mock.expects(:get).once
-           .with('https://api.stripe.com/v1/apple_pay/domains/apwc_test_domain', nil, nil)
-           .returns(make_response(make_apple_pay_domain))
-      @mock.expects(:delete).once.returns(make_response(make_apple_pay_domain(:deleted => true)))
-      domain = Stripe::ApplePayDomain.retrieve('apwc_test_domain')
-      domain.delete
-      assert domain.deleted
+    should "create should return a new Apple Pay domain" do
+      Stripe::ApplePayDomain.create(domain_name: "example.com")
+      assert_requested :post, "#{Stripe.api_url}/v1/apple_pay/domains"
     end
 
     should "domains should be listable" do
-      @mock.expects(:get).once.with('https://api.stripe.com/v1/apple_pay/domains', nil, nil)
-           .returns(make_response(make_apple_pay_domain_array))
-      domains = Stripe::ApplePayDomain.list
-      assert domains.data.kind_of?(Array)
-      assert_equal 3, domains.data.length
-      domains.each do |domain|
-        assert domain.kind_of?(Stripe::ApplePayDomain)
-      end
+      Stripe::ApplePayDomain.list
+      assert_requested :get, "#{Stripe.api_url}/v1/apple_pay/domains"
+    end
+
+    should "domains should be retrievable" do
+      domain = Stripe::ApplePayDomain.list.first
+      domain = Stripe::ApplePayDomain.retrieve(domain.id)
+      assert_requested :get, "#{Stripe.api_url}/v1/apple_pay/domains/#{domain.id}"
+    end
+
+    should "domains should be deletable" do
+      domain = Stripe::ApplePayDomain.list.first
+      domain.delete
+      assert_requested :delete, "#{Stripe.api_url}/v1/apple_pay/domains/#{domain.id}"
     end
   end
 end

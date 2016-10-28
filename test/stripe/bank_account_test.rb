28c58ff212ad28c54c1b9fa7d73eec24cee66b53
Brandur <brandur@mutelight.org>
2016-10-27 17:24:10 -0700

    Convert suites for bank account + bitcoin receiver

--- test/stripe/bank_account_test.rb
+++ test/stripe/bank_account_test.rb
@@ -2,20 +2,19 @@ require File.expand_path('../../test_helper', __FILE__)
 
 module Stripe
   class BankAccountTest < Test::Unit::TestCase
+    include WithoutLegacyStubs
 
-    should 'be verifiable' do
-      bank = Stripe::BankAccount.construct_from({
-        :id => 'ba_foo',
-        :customer => 'cus_bar'
+    BANK_ACCOUNT_ID = "ba_1234"
+    CUSTOMER_ID = "cus_1234"
+
+    should "be verifiable" do
+      account = Stripe::BankAccount.construct_from({
+        :id => BANK_ACCOUNT_ID,
+        :customer => CUSTOMER_ID,
       })
+      account.verify(:amounts => [1,2])
 
-      @mock.expects(:post).
-        once.
-        with('https://api.stripe.com/v1/customers/cus_bar/sources/ba_foo/verify', nil, 'amounts[]=1&amounts[]=2').
-        returns(make_response(:status => 'verified'))
-
-      bank.verify(:amounts => [1,2])
+      assert_requested :post, "#{Stripe.api_url}/v1/customers/#{CUSTOMER_ID}/sources/#{BANK_ACCOUNT_ID}/verify"
     end
-
   end
 end
--- test/stripe/bitcoin_receiver_test.rb
+++ test/stripe/bitcoin_receiver_test.rb
@@ -2,60 +2,68 @@ require File.expand_path('../../test_helper', __FILE__)
 
 module Stripe
   class BitcoinReceiverTest < Test::Unit::TestCase
-    should "retrieve should retrieve bitcoin receiver" do
-      @mock.expects(:get).once.returns(make_response(make_bitcoin_receiver))
-      receiver = Stripe::BitcoinReceiver.retrieve('btcrcv_test_receiver')
-      assert_equal 'btcrcv_test_receiver', receiver.id
+    include WithoutLegacyStubs
+
+    CUSTOMER_ID = "cus_1234"
+    RECEIVER_ID = "btcrcv_1234"
+
+    should "be creatable" do
+      receiver = Stripe::BitcoinReceiver.create(
+        :amount => 1234,
+        :currency => "USD"
+      )
+      assert_kind_of Stripe::BitcoinReceiver, receiver
+      assert_requested :post, "#{Stripe.api_url}/v1/bitcoin/receivers"
     end
 
-    should "create should create a bitcoin receiver" do
-      @mock.expects(:post).once.returns(make_response(make_bitcoin_receiver))
-      receiver = Stripe::BitcoinReceiver.create
-      assert_equal "btcrcv_test_receiver", receiver.id
-    end
-
-    should "all should list bitcoin receivers" do
-      @mock.expects(:get).once.returns(make_response(make_bitcoin_receiver_array))
+    should "be listable" do
       receivers = Stripe::BitcoinReceiver.list
-      assert_equal 3, receivers.data.length
-      assert receivers.data.kind_of? Array
-      receivers.each do |receiver|
-        assert receiver.kind_of?(Stripe::BitcoinReceiver)
-        receiver.transactions.data.each do |transaction|
-          assert transaction.kind_of?(Stripe::BitcoinTransaction)
-        end
-      end
+      assert_kind_of Stripe::BitcoinReceiver, receivers.first
+      assert_requested :get, "#{Stripe.api_url}/v1/bitcoin/receivers"
     end
 
-    should "maintain bitcoin transaction sublist" do
-      @mock.expects(:get).with("#{Stripe.api_base}/v1/bitcoin/receivers/btcrcv_test_receiver", nil, nil).once.returns(make_response(make_bitcoin_receiver))
-      receiver = Stripe::BitcoinReceiver.retrieve('btcrcv_test_receiver')
-      @mock.expects(:get).with("#{Stripe.api_base}/v1/bitcoin/receivers/btcrcv_test_receiver/transactions", nil, nil).once.returns(make_response(make_bitcoin_transaction_array))
-      transactions = receiver.transactions.list
-      assert_equal(3, transactions.data.length)
+    should "be retrievable" do
+      receiver = Stripe::BitcoinReceiver.retrieve("btcrcv_1234")
+      assert_kind_of Stripe::BitcoinReceiver, receiver
+      assert_requested :get, "#{Stripe.api_url}/v1/bitcoin/receivers/btcrcv_1234"
     end
 
-    should "update should update a bitcoin receiver" do
-      @mock.expects(:get).once.returns(make_response(make_bitcoin_receiver))
-      @mock.expects(:post).with("#{Stripe.api_base}/v1/bitcoin/receivers/btcrcv_test_receiver", nil, "description=details").once.returns(make_response(make_bitcoin_receiver))
-      receiver = Stripe::BitcoinReceiver.construct_from(make_bitcoin_receiver)
-      receiver.refresh
-      receiver.description = "details"
+    should "be updatable" do
+      # without customer
+      receiver = Stripe::BitcoinReceiver.construct_from({
+        :id => RECEIVER_ID,
+        :customer => nil,
+      })
       receiver.save
+      assert_requested :post, "#{Stripe.api_url}/v1/bitcoin/receivers/#{RECEIVER_ID}"
+
+      # with customer
+      receiver = Stripe::BitcoinReceiver.construct_from({
+        :id => RECEIVER_ID,
+        :customer => CUSTOMER_ID,
+      })
+      receiver.save
+      assert_requested :post, "#{Stripe.api_url}/v1/customers/#{CUSTOMER_ID}/sources/#{RECEIVER_ID}"
     end
 
-    should "delete a bitcoin receiver with no customer through top-level API" do
-      @mock.expects(:delete).with("#{Stripe.api_base}/v1/bitcoin/receivers/btcrcv_test_receiver", nil, nil).once.returns(make_response({:deleted => true, :id => "btcrcv_test_receiver"}))
-      receiver = Stripe::BitcoinReceiver.construct_from(make_bitcoin_receiver)
-      receiver.delete
-      assert(receiver.deleted)
-    end
+    should "be deletable" do
+      # without customer
+      receiver = Stripe::BitcoinReceiver.construct_from({
+        :id => RECEIVER_ID,
+        :customer => nil,
+      })
+      receiver = receiver.delete
+      assert_kind_of Stripe::BitcoinReceiver, receiver
+      assert_requested :delete, "#{Stripe.api_url}/v1/bitcoin/receivers/#{RECEIVER_ID}"
 
-    should "delete a bitcoin receiver with a customer through customer's subresource API" do
-      @mock.expects(:delete).with("#{Stripe.api_base}/v1/customers/customer_foo/sources/btcrcv_test_receiver", nil, nil).once.returns(make_response({:deleted => true, :id => "btcrcv_test_receiver"}))
-      receiver = Stripe::BitcoinReceiver.construct_from(make_bitcoin_receiver(:customer => 'customer_foo'))
-      receiver.delete
-      assert(receiver.deleted)
+      # with customer
+      receiver = Stripe::BitcoinReceiver.construct_from({
+        :id => RECEIVER_ID,
+        :customer => CUSTOMER_ID,
+      })
+      receiver = receiver.delete
+      assert_kind_of Stripe::BitcoinReceiver, receiver
+      assert_requested :delete, "#{Stripe.api_url}/v1/customers/#{CUSTOMER_ID}/sources/#{RECEIVER_ID}"
     end
   end
 end

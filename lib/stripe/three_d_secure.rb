4122f9bbb335ddf461a23a6bc20345dd84775cbd
Olivier Bellone <ob@stripe.com>
2016-11-21 19:36:58 +0100

    Adds 3DS retrieve endpoint

--- lib/stripe/three_d_secure.rb
+++ lib/stripe/three_d_secure.rb
@@ -5,9 +5,5 @@ module Stripe
     def self.resource_url
       "/v1/3d_secure"
     end
-
-    def self.retrieve(id, opts=nil)
-      raise NotImplementedError.new("3D Secure objects cannot be retrieved")
-    end
   end
 end

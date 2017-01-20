7c3295d7582583a48d10cef1848693cb3e022140
Brandur <brandur@mutelight.org>
2017-01-19 16:10:48 -0800

    More tests converted

--- test/stripe/coupon_test.rb
+++ test/stripe/coupon_test.rb
@@ -3,26 +3,28 @@ require File.expand_path('../../test_helper', __FILE__)
 module Stripe
   class CouponTest < Test::Unit::TestCase
     should "create should return a new coupon" do
-      @mock.expects(:post).once.returns(make_response(make_coupon))
-      c = Stripe::Coupon.create
-      assert_equal "co_test_coupon", c.id
+      stub_request(:post, "#{Stripe.api_base}/v1/coupons").
+        to_return(body: make_response(make_coupon))
+      _ = Stripe::Coupon.create
     end
 
     should "coupons should be saveable" do
-      @mock.expects(:get).once.returns(make_response(make_coupon))
-      @mock.expects(:post).once.returns(make_response(make_coupon))
-      c = Stripe::Coupon.new("test_coupon")
-      c.refresh
+      stub_request(:get, "#{Stripe.api_base}/v1/coupons/test_coupon").
+        to_return(body: make_response(make_coupon))
+      c = Stripe::Coupon.retrieve("test_coupon")
+
+      stub_request(:post, "#{Stripe.api_base}/v1/coupons/#{c.id}").
+        with(body: { metadata: { foo: "bar" } }).
+        to_return(body: make_response(make_customer))
       c.metadata['foo'] = 'bar'
       c.save
     end
 
     should "coupons should be updateable" do
-      @mock.expects(:post).once.
-        with("https://api.stripe.com/v1/coupons/test_coupon", nil, "metadata[foo]=bar").
-        returns(make_response(make_coupon(metadata: {foo: 'bar'})))
-      c = Stripe::Coupon.update("test_coupon", metadata: {foo: 'bar'})
-      assert_equal('bar', c.metadata['foo'])
+      stub_request(:post, "#{Stripe.api_base}/v1/coupons/test_coupon").
+        with(body: { metadata: { foo: "bar" } }).
+        to_return(body: make_response(make_customer))
+      _ = Stripe::Coupon.update("test_coupon", metadata: {foo: 'bar'})
     end
   end
 end

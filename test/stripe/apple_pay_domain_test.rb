d3d085f72f5c65e8a25fc2c3c0b851debfa4a77d
Brandur <brandur@mutelight.org>
2017-01-26 16:56:32 -0800

    Modernize ApplePayDomain test

---
+++ test/stripe/apple_pay_domain_test.rb
@@ -0,0 +1,31 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+module Stripe
+  class ApplePayDomainTest < Test::Unit::TestCase
+    FIXTURE = API_FIXTURES[:apple_pay_domain]
+
+    should "be listable" do
+      domains = Stripe::ApplePayDomain.list
+      assert_requested :get, "#{Stripe.api_base}/v1/apple_pay/domains"
+      assert domains.data.kind_of?(Array)
+      assert domains.data[0].kind_of?(Stripe::ApplePayDomain)
+    end
+
+    should "be retrievable" do
+      domain = Stripe::ApplePayDomain.retrieve(FIXTURE[:id])
+      assert_requested :get, "#{Stripe.api_base}/v1/apple_pay/domains/#{FIXTURE[:id]}"
+      assert domain.kind_of?(Stripe::ApplePayDomain)
+    end
+
+    should "be creatable" do
+      _ = Stripe::ApplePayDomain.create(:domain_name => "example.com")
+      assert_requested :post, "#{Stripe.api_base}/v1/apple_pay/domains"
+    end
+
+    should "be deletable" do
+      domain = Stripe::ApplePayDomain.retrieve(FIXTURE[:id])
+      domain.delete
+      assert_requested :delete, "#{Stripe.api_base}/v1/apple_pay/domains/#{FIXTURE[:id]}"
+    end
+  end
+end

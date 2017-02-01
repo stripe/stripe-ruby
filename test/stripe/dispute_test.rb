508ca4d2f1e694aa1d09db96022e1462835da9c3
Brandur <brandur@mutelight.org>
2017-02-01 10:01:04 -0800

    Migrate dispute tests

---
+++ test/stripe/dispute_test.rb
@@ -0,0 +1,40 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+module Stripe
+  class DisputeTest < Test::Unit::TestCase
+    FIXTURE = API_FIXTURES.fetch(:dispute)
+
+    should "be listable" do
+      disputes = Stripe::Dispute.list
+      assert_requested :get, "#{Stripe.api_base}/v1/disputes"
+      assert disputes.data.kind_of?(Array)
+      assert disputes.first.kind_of?(Stripe::Dispute)
+    end
+
+    should "be retrievable" do
+      dispute = Stripe::Dispute.retrieve(FIXTURE[:id])
+      assert_requested :get, "#{Stripe.api_base}/v1/disputes/#{FIXTURE[:id]}"
+      assert dispute.kind_of?(Stripe::Dispute)
+    end
+
+    should "be saveable" do
+      dispute = Stripe::Dispute.retrieve(FIXTURE[:id])
+      dispute.metadata['key'] = 'value'
+      dispute.save
+      assert_requested :post, "#{Stripe.api_base}/v1/disputes/#{FIXTURE[:id]}"
+    end
+
+    should "be updateable" do
+      dispute = Stripe::Dispute.update(FIXTURE[:id], metadata: { key: 'value' })
+      assert_requested :post, "#{Stripe.api_base}/v1/disputes/#{FIXTURE[:id]}"
+      assert dispute.kind_of?(Stripe::Dispute)
+    end
+
+    should "be closeable" do
+      dispute = Stripe::Dispute.retrieve(FIXTURE[:id])
+      dispute.close
+      assert_requested :post,
+        "#{Stripe.api_base}/v1/disputes/#{FIXTURE[:id]}/close"
+    end
+  end
+end

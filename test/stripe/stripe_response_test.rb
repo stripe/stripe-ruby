5213a9f8c971afcb5ea238f69e71689f3bfe436b
Brandur <brandur@mutelight.org>
2017-02-01 12:38:29 -0800

    Stripe response tests don't need migration

--- test/stripe/stripe_response_test.rb
+++ test/stripe/stripe_response_test.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../../../test_helper', __FILE__)
+require File.expand_path('../../test_helper', __FILE__)
 
 module Stripe
   class StripeResponseTest < Test::Unit::TestCase

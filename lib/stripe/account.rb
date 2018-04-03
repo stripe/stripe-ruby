2e20ef421897d132386ebb0093c748d67c629698
Jacob Senecal <jacob.senecal@gmail.com>
2018-04-03 16:08:27 -0500

    Improve syntax for checking transfer schedule interval

--- lib/stripe/account.rb
+++ lib/stripe/account.rb
@@ -83,9 +83,9 @@ module Stripe
         end
       end
       if (transfer_schedule = update_hash[:transfer_schedule])
-        if (transfer_schedule[:interval] == "weekly")
+        if transfer_schedule[:interval] != "monthly"
           transfer_schedule.delete(:monthly_anchor)
-        elsif (transfer_schedule[:interval] == "monthly")
+        elsif transfer_schedule[:interval] != "weekly"
           transfer_schedule.delete(:weekly_anchor)
         end
       end

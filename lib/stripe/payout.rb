6592c0eb15e1d4fe0dd4175b0760b54b96550b63
Corey Farwell <coreyf@rwell.org>
2018-04-19 13:51:41 +0900

    Add status constants to Stripe::Payout.

--- lib/stripe/payout.rb
+++ lib/stripe/payout.rb
@@ -6,6 +6,14 @@ module Stripe
 
     OBJECT_NAME = "payout".freeze
 
+    STATUSES = [
+      STATUS_PAID = "paid".freeze,
+      STATUS_PENDING = "pending".freeze,
+      STATUS_IN_TRANSIT = "in_transit".freeze,
+      STATUS_CANCELED = "canceled".freeze,
+      STATUS_FAILED = "failed".freeze,
+    ].freeze
+
     def cancel
       resp, api_key = request(:post, cancel_url)
       initialize_from(resp.data, api_key)

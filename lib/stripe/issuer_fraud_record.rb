617801e2883d83af07dcbede2cec52b8c92b2f30
Fay Wu <fay@stripe.com>
2018-05-07 15:06:51 -0700

    Add resource url to issuer fraud record

--- lib/stripe/issuer_fraud_record.rb
+++ lib/stripe/issuer_fraud_record.rb
@@ -3,5 +3,9 @@ module Stripe
     extend Stripe::APIOperations::List
 
     OBJECT_NAME = "issuer_fraud_record".freeze
+
+    def self.resource_url
+      "/v1/issuer_fraud_records"
+    end
   end
 end

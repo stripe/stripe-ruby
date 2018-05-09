cdc3fd00860c9d84ad5ee65ac6ba289b71647520
Fay Wu <fay@stripe.com>
2018-05-09 11:43:28 -0700

    Add listing by charge ID

--- lib/stripe/issuer_fraud_record.rb
+++ lib/stripe/issuer_fraud_record.rb
@@ -4,6 +4,16 @@ module Stripe
 
     OBJECT_NAME = "issuer_fraud_record".freeze
 
+    def self.from_charge(params, opts = {})
+      resp, opts = request(:get, from_charge_url, params, opts)
+      resp.data
+    end
+
+    def self.from_charge_url
+      resource_url
+    end
+    private_class_method :from_charge_url
+
     def self.resource_url
       "/v1/issuer_fraud_records"
     end

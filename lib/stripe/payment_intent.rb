201f9c29f4df51dc0898d73b18eae15848d958aa
Remi Jannel <remi@stripe.com>
2018-06-27 16:36:14 -0400

    Add support for the PaymentIntent resource
    This feature is gated so the tests are stubbed for now

---
+++ lib/stripe/payment_intent.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Stripe
+  class PaymentIntent < APIResource
+    extend Stripe::APIOperations::Create
+    include Stripe::APIOperations::Delete
+    extend Stripe::APIOperations::List
+    include Stripe::APIOperations::Save
+
+    OBJECT_NAME = "payment_intent".freeze
+
+    def self.resource_url
+      "/v1/payment_intents"
+    end
+
+    def cancel
+      resp, api_key = request(:post, resource_url + "/cancel")
+      initialize_from(resp.data, api_key)
+    end
+
+    def capture
+      resp, api_key = request(:post, resource_url + "/capture")
+      initialize_from(resp.data, api_key)
+    end
+
+    def confirm
+      resp, api_key = request(:post, resource_url + "/confirm")
+      initialize_from(resp.data, api_key)
+    end
+  end
+end

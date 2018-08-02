bc20977ad4d4da6fc846be0bfd1585518da7cac1
Forest Hu <foresthu@stripe.com>
2018-08-02 15:41:41 -0700

    Add cancel support for topups

--- lib/stripe/topup.rb
+++ lib/stripe/topup.rb
@@ -7,5 +7,14 @@ module Stripe
     include Stripe::APIOperations::Save
 
     OBJECT_NAME = "topup".freeze
+
+    def cancel
+      resp, api_key = request(:post, cancel_url)
+      initialize_from(resp.data, api_key)
+    end
+
+    def cancel_url
+      resource_url + "/cancel"
+    end
   end
 end

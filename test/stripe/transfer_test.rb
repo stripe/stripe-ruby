5aa63d2cc63a3ccf7354b0b59760728b19c9f667
Remi Jannel <remi@stripe.com>
2019-04-12 19:23:52 -0700

    Move tests for nested resources up to the parent class

--- test/stripe/transfer_test.rb
+++ test/stripe/transfer_test.rb
@@ -39,5 +39,50 @@ module Stripe
       assert_requested :post, "#{Stripe.api_base}/v1/transfers/tr_123"
       assert transfer.is_a?(Stripe::Transfer)
     end
+
+    context "#create_reversal" do
+      should "create a reversal" do
+        reversal = Stripe::Transfer.create_reversal(
+          "tr_123",
+          amount: 100
+        )
+        assert_requested :post, "#{Stripe.api_base}/v1/transfers/tr_123/reversals"
+        assert reversal.is_a?(Stripe::Reversal)
+      end
+    end
+
+    context "#retrieve_reversal" do
+      should "retrieve a reversal" do
+        reversal = Stripe::Transfer.retrieve_reversal(
+          "tr_123",
+          "trr_123"
+        )
+        assert_requested :get, "#{Stripe.api_base}/v1/transfers/tr_123/reversals/trr_123"
+        assert reversal.is_a?(Stripe::Reversal)
+      end
+    end
+
+    context "#update_reversal" do
+      should "update a reversal" do
+        reversal = Stripe::Transfer.update_reversal(
+          "tr_123",
+          "trr_123",
+          metadata: { foo: "bar" }
+        )
+        assert_requested :post, "#{Stripe.api_base}/v1/transfers/tr_123/reversals/trr_123"
+        assert reversal.is_a?(Stripe::Reversal)
+      end
+    end
+
+    context "#list_reversals" do
+      should "list the transfer's reversals" do
+        reversals = Stripe::Transfer.list_reversals(
+          "tr_123"
+        )
+        assert_requested :get, "#{Stripe.api_base}/v1/transfers/tr_123/reversals"
+        assert reversals.is_a?(Stripe::ListObject)
+        assert reversals.data.is_a?(Array)
+      end
+    end
   end
 end

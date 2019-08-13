b87c5a512f2a94e7a113687fbe8c29afb2ea454d
Olivier Bellone <ob@stripe.com>
2019-08-13 13:04:23 -0700

    Remove UsageRecord.create method (#826)

--- lib/stripe/resources/usage_record.rb
+++ lib/stripe/resources/usage_record.rb
@@ -3,22 +3,5 @@
 module Stripe
   class UsageRecord < APIResource
     OBJECT_NAME = "usage_record"
-
-    def self.create(params = {}, opts = {})
-      unless params.key?(:subscription_item)
-        raise ArgumentError, "Params must have a subscription_item key"
-      end
-
-      req_params = params.clone.delete_if do |key, _value|
-        key == :subscription_item
-      end
-      resp, opts = request(
-        :post,
-        "/v1/subscription_items/#{params[:subscription_item]}/usage_records",
-        req_params,
-        opts
-      )
-      Util.convert_to_stripe_object(resp.data, opts)
-    end
   end
 end

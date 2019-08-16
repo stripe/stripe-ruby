e840ce89bc9641ebc64b440e5c11be42714ffd46
Olivier Bellone <ob@stripe.com>
2019-08-16 10:18:48 -0700

    Add ErrorObject to StripeError exceptions (#811)

--- test/stripe/errors_test.rb
+++ test/stripe/errors_test.rb
@@ -4,16 +4,37 @@ require ::File.expand_path("../test_helper", __dir__)
 
 module Stripe
   class StripeErrorTest < Test::Unit::TestCase
-    context "#to_s" do
-      should "convert to string" do
-        e = StripeError.new("message")
-        assert_equal "message", e.to_s
+    context "StripeError" do
+      context "#initialize" do
+        should "initialize error if json_body is set" do
+          e = StripeError.new("message", json_body: { error: { code: "some_error" } })
+          assert_not_nil e.error
+          assert_equal "some_error", e.error.code
+          assert_nil e.error.charge
+        end
+      end
 
-        e = StripeError.new("message", http_status: 200)
-        assert_equal "(Status 200) message", e.to_s
+      context "#to_s" do
+        should "convert to string" do
+          e = StripeError.new("message")
+          assert_equal "message", e.to_s
 
-        e = StripeError.new("message", http_status: nil, http_body: nil, json_body: nil, http_headers: { request_id: "request-id" })
-        assert_equal "(Request request-id) message", e.to_s
+          e = StripeError.new("message", http_status: 200)
+          assert_equal "(Status 200) message", e.to_s
+
+          e = StripeError.new("message", http_status: nil, http_body: nil, json_body: nil, http_headers: { request_id: "request-id" })
+          assert_equal "(Request request-id) message", e.to_s
+        end
+      end
+    end
+
+    context "OAuth::OAuthError" do
+      context "#initialize" do
+        should "initialize error if json_body is set" do
+          e = OAuth::OAuthError.new("message", "description", json_body: { error: "some_oauth_error" })
+          assert_not_nil e.error
+          assert_equal "some_oauth_error", e.error.error
+        end
       end
     end
   end

87591700160d5647d8e3aa4e40185bb1bfdc1b7e
Olivier Bellone <ob@stripe.com>
2019-11-26 12:43:19 -0800

    Codegen for openapi ab6898a

--- lib/stripe/resources/credit_note.rb
+++ lib/stripe/resources/credit_note.rb
@@ -8,7 +8,17 @@ module Stripe
 
     OBJECT_NAME = "credit_note"
 
+    custom_method :preview, http_verb: :get
     custom_method :void_credit_note, http_verb: :post, http_path: "void"
+
+    def preview(params = {}, opts = {})
+      request_stripe_object(
+        method: :get,
+        path: resource_url + "/preview",
+        params: params,
+        opts: opts
+      )
+    end
 
     def void_credit_note(params = {}, opts = {})
       request_stripe_object(

1b0fce5f683863dbedf55ca938ffb28b4103b151
Richard Marmorstein <richardm@stripe.com>
2021-07-12 18:12:56 -0400

    Codegen for openapi 265b165

--- lib/stripe/resources/quote.rb
+++ lib/stripe/resources/quote.rb
@@ -12,6 +12,7 @@ module Stripe
     custom_method :accept, http_verb: :post
     custom_method :cancel, http_verb: :post
     custom_method :finalize_quote, http_verb: :post, http_path: "finalize"
+    custom_method :list_computed_upfront_line_items, http_verb: :get, http_path: "computed_upfront_line_items"
     custom_method :list_line_items, http_verb: :get, http_path: "line_items"
 
     def accept(params = {}, opts = {})
@@ -36,6 +37,15 @@ module Stripe
       request_stripe_object(
         method: :post,
         path: resource_url + "/finalize",
+        params: params,
+        opts: opts
+      )
+    end
+
+    def list_computed_upfront_line_items(params = {}, opts = {})
+      request_stripe_object(
+        method: :get,
+        path: resource_url + "/computed_upfront_line_items",
         params: params,
         opts: opts
       )

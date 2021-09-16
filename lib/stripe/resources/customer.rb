41ec2d175c69515b1452cdea544ffea0fcf43171
Dominic Charley-Roy <dcr@stripe.com>
2021-09-16 15:20:30 -0400

    Codegen for openapi 151a466

--- lib/stripe/resources/customer.rb
+++ lib/stripe/resources/customer.rb
@@ -9,18 +9,18 @@ module Stripe
     include Stripe::APIOperations::Save
     extend Stripe::APIOperations::NestedResource
 
-    OBJECT_NAME = "customer"
+    OBJECT_NAME = 'customer'
 
     nested_resource_class_methods :balance_transaction,
-                                  operations: %i[create retrieve update list]
+      operations: %i[create retrieve update list]
     nested_resource_class_methods :tax_id,
-                                  operations: %i[create retrieve delete list]
+      operations: %i[create retrieve delete list]
 
     custom_method :delete_discount, http_verb: :delete, http_path: "discount"
 
     save_nested_resource :source
     nested_resource_class_methods :source,
-                                  operations: %i[create retrieve update delete list]
+      operations: %i[create retrieve update delete list]
 
     # The API request for deleting a card or bank account and for detaching a
     # source object are the same.

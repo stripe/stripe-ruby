59f2d4d0fe0d3dc03a8fbd5bd9a0d4fd8bef230b
Pavel Krymets <pakrym@stripe.com>
2022-08-11 15:03:49 -0700

    suppress linting error

--- lib/stripe/api_version.rb
+++ lib/stripe/api_version.rb
@@ -3,6 +3,8 @@
 
 module Stripe
   module ApiVersion
+    # rubocop:disable Layout/LineLength
     CURRENT = "2022-08-01; server_side_confirmation_beta=v1; orders_beta=v4; terminal_interac_refunds_beta=v1"
+    # rubocop:enable Layout/LineLength
   end
 end

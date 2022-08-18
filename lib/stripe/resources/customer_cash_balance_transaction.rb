ff7d284be0ec42915bca10fcf95d08d7bd91a0f6
Kamil Pajdzik <kamil@stripe.com>
2022-08-18 16:03:24 -0700

    Codegen for openapi v183

---
+++ lib/stripe/resources/customer_cash_balance_transaction.rb
@@ -0,0 +1,10 @@
+# File generated from our OpenAPI spec
+# frozen_string_literal: true
+
+module Stripe
+  class CustomerCashBalanceTransaction < APIResource
+    extend Stripe::APIOperations::List
+
+    OBJECT_NAME = "customer_cash_balance_transaction"
+  end
+end

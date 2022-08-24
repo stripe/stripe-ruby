f6474127896c7d0e812f9f0e6b6cd2aadaf682dd
Annie Li <anniel@stripe.com>
2022-08-24 10:56:47 -0700

    Add comment and change to starts_with

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-if RUBY_VERSION >= "3.1"
+# Report test coverage to coveralls for only one Ruby version to avoid
+# repeated builds. This also accounts for coveralls_reborn requiring
+# RUBY_VERSION >= 2.5.
+if RUBY_VERSION.start_with?("3.1")
   require "coveralls"
   Coveralls.wear!
 end

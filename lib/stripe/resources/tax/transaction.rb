9a4dfdcb2fa74490561a8b242ecd824e95d0510c
Annie Li <anniel@stripe.com>
2023-01-26 10:51:25 -0800

    Codegen for openapi v221

--- lib/stripe/resources/tax/transaction.rb
+++ lib/stripe/resources/tax/transaction.rb
@@ -9,10 +9,37 @@ module Stripe
 
       OBJECT_NAME = "tax.transaction"
 
+      def list_line_items(params = {}, opts = {})
+        request_stripe_object(
+          method: :get,
+          path: format("/v1/tax/transactions/%<transaction>s/line_items", { transaction: CGI.escape(self["id"]) }),
+          params: params,
+          opts: opts
+        )
+      end
+
       def self.create_reversal(params = {}, opts = {})
         request_stripe_object(
           method: :post,
           path: "/v1/tax/transactions/create_reversal",
+          params: params,
+          opts: opts
+        )
+      end
+
+      def self.list_line_items(transaction, params = {}, opts = {})
+        request_stripe_object(
+          method: :get,
+          path: format("/v1/tax/transactions/%<transaction>s/line_items", { transaction: CGI.escape(transaction) }),
+          params: params,
+          opts: opts
+        )
+      end
+
+      def self.list_transactions(params = {}, opts = {})
+        request_stripe_object(
+          method: :get,
+          path: "/v1/tax/transactions",
           params: params,
           opts: opts
         )

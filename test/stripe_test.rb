fc34c16496819f4d35e75dadc335d87f45b049aa
Annie Li <anniel@stripe.com>
2023-05-10 11:25:38 -0700

    better tests

--- test/stripe_test.rb
+++ test/stripe_test.rb
@@ -184,7 +184,7 @@ class StripeTest < Test::Unit::TestCase
       assert_equal expected_body, resp.http_body
     end
 
-    should "send get request with json body" do
+    should "send get request with json body and headers and return a response" do
       expected_body = "{\"id\": \"acc_123\"}"
       req = nil
 
@@ -194,8 +194,72 @@ class StripeTest < Test::Unit::TestCase
 
       resp = Stripe.raw_request(:get, "/v1/accounts/acc_123", {}, { api_mode: :preview, "Stripe-Account": "bar" })
 
-      assert_not_equal req.headers["Content-Type"], "application/x-www-form-urlencoded"
-      assert_equal resp.http_body, expected_body
+      assert_not_equal "application/x-www-form-urlencoded", req.headers["Content-Type"]
+      assert_equal expected_body, resp.http_body
+    end
+  end
+
+  context "preview raw requests" do
+    should "send preview get request with correct default options" do
+      expected_body = "{\"id\": \"acc_123\"}"
+      req = nil
+
+      stub_request(:get, "#{Stripe.api_base}/v2/accounts/acc_123")
+        .with { |request| req = request }
+        .to_return(body: expected_body)
+
+      resp = Stripe::Preview.get("/v2/accounts/acc_123")
+
+      assert_not_equal "application/x-www-form-urlencoded", req.headers["Content-Type"]
+      assert_equal Stripe::Preview::PREVIEW_API_VERSION, req.headers["Stripe-Version"]
+      assert_equal expected_body, resp.http_body
+    end
+
+    should "send preview post request with correct default options" do
+      expected_body = "{\"id\": \"acc_123\"}"
+      req = nil
+
+      stub_request(:post, "#{Stripe.api_base}/v2/accounts")
+        .with { |request| req = request }
+        .to_return(body: expected_body)
+
+      resp = Stripe::Preview.post("/v2/accounts", { p1: 1, p2: "string" })
+
+      assert_equal "application/json", req.headers["Content-Type"]
+      assert_equal Stripe::Preview::PREVIEW_API_VERSION, req.headers["Stripe-Version"]
+      assert_equal "{\"p1\":1,\"p2\":\"string\"}", req.body
+      assert_equal expected_body, resp.http_body
+    end
+
+    should "send preview delete request with correct default options" do
+      expected_body = "{\"id\": \"acc_123\"}"
+      req = nil
+
+      stub_request(:delete, "#{Stripe.api_base}/v2/accounts/acc_123")
+        .with { |request| req = request }
+        .to_return(body: expected_body)
+
+      resp = Stripe::Preview.delete("/v2/accounts/acc_123")
+
+      assert_not_equal "application/x-www-form-urlencoded", req.headers["Content-Type"]
+      assert_equal Stripe::Preview::PREVIEW_API_VERSION, req.headers["Stripe-Version"]
+      assert_equal expected_body, resp.http_body
+    end
+
+    should "allow overriding default options for preview requests" do
+      expected_body = "{\"id\": \"acc_123\"}"
+      stripe_version_override = "2022-11-15"
+      req = nil
+
+      stub_request(:post, "#{Stripe.api_base}/v2/accounts")
+        .with { |request| req = request }
+        .to_return(body: expected_body)
+
+      resp = Stripe::Preview.post("/v2/accounts", {}, { stripe_version: stripe_version_override })
+
+      assert_not_equal "application/x-www-form-urlencoded", req.headers["Content-Type"]
+      assert_equal stripe_version_override, req.headers["Stripe-Version"]
+      assert_equal expected_body, resp.http_body
     end
   end
 

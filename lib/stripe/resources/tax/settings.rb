98d0ee99ebe7d6853fdf0e2619c8ca3f08862c82
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-07-11 17:21:02 +0000

    Update generated code for v413

---
+++ lib/stripe/resources/tax/settings.rb
@@ -0,0 +1,15 @@
+# File generated from our OpenAPI spec
+# frozen_string_literal: true
+
+module Stripe
+  module Tax
+    # You can use Tax `Settings` to manage configurations used by Stripe Tax calculations.
+    #
+    # Related guide: [Using the Settings API](https://stripe.com/docs/tax/settings-api)
+    class Settings < SingletonAPIResource
+      include Stripe::APIOperations::Save
+
+      OBJECT_NAME = "tax.settings"
+    end
+  end
+end

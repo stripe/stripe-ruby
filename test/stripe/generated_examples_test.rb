4aa9968717953663d073f3efa1184dea770c0649
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-09-11 18:58:36 +0000

    Update generated code for v522

--- test/stripe/generated_examples_test.rb
+++ test/stripe/generated_examples_test.rb
@@ -1,6 +1,6 @@
+# File generated from our OpenAPI spec
 # frozen_string_literal: true
 
-# File generated from our OpenAPI spec
 require ::File.expand_path("../test_helper", __dir__)
 module Stripe
   class CodegennedExampleTest < Test::Unit::TestCase

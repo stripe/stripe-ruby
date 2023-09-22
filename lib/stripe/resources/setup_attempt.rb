3659c7eb0f78d0d0c6554adcbd543d83871b6941
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-09-22 16:55:35 +0000

    Update generated code for v548

--- lib/stripe/resources/setup_attempt.rb
+++ lib/stripe/resources/setup_attempt.rb
@@ -3,7 +3,7 @@
 
 module Stripe
   # A SetupAttempt describes one attempted confirmation of a SetupIntent,
-  # whether that confirmation was successful or unsuccessful. You can use
+  # whether that confirmation is successful or unsuccessful. You can use
   # SetupAttempts to inspect details of a specific attempt at setting up a
   # payment method using a SetupIntent.
   class SetupAttempt < APIResource

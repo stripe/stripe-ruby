05ee1f8fd4fbd809c53afd24a676e7e54f51456d
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2023-10-02 15:36:11 +0000

    Update generated code for v573

--- lib/stripe/resources/payment_method_configuration.rb
+++ lib/stripe/resources/payment_method_configuration.rb
@@ -2,7 +2,20 @@
 # frozen_string_literal: true
 
 module Stripe
-  # An object detailing payment method configurations.
+  # PaymentMethodConfigurations control which payment methods are displayed to your customers when you don't explicitly specify payment method types. You can have multiple configurations with different sets of payment methods for different scenarios.
+  #
+  # There are two types of PaymentMethodConfigurations. Which is used depends on the [charge type](https://stripe.com/docs/connect/charges):
+  #
+  # **Direct** configurations apply to payments created on your account, including Connect destination charges, Connect separate charges and transfers, and payments not involving Connect.
+  #
+  # **Child** configurations apply to payments created on your connected accounts using direct charges, and charges with the on_behalf_of parameter.
+  #
+  # Child configurations have a `parent` that sets default values and controls which settings connected accounts may override. You can specify a parent ID at payment time, and Stripe will automatically resolve the connected account's associated child configuration. Parent configurations are [managed in the dashboard](https://dashboard.stripe.com/settings/payment_methods/connected_accounts) and are not available in this API.
+  #
+  # Related guides:
+  # - [Payment Method Configurations API](https://stripe.com/docs/connect/payment-method-configurations)
+  # - [Multiple payment method configurations on dynamic payment methods](https://stripe.com/docs/payments/multiple-payment-method-configs)
+  # - [Multiple configurations for your Connect accounts](https://stripe.com/docs/connect/multiple-payment-method-configurations)
   class PaymentMethodConfiguration < APIResource
     extend Stripe::APIOperations::Create
     extend Stripe::APIOperations::List

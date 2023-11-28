580a37755a9ce88e9b276850ac209a8138668d99
Richard Marmorstein <52928443+richardm-stripe@users.noreply.github.com>
2023-11-28 15:29:56 -0800

    Update lib/stripe/stripe_client.rb
    Co-authored-by: anniel-stripe <97691964+anniel-stripe@users.noreply.github.com>

--- lib/stripe/stripe_client.rb
+++ lib/stripe/stripe_client.rb
@@ -1038,6 +1038,7 @@ module Stripe
       # Request duration in milliseconds
       attr_accessor :request_duration_ms
 
+      # a list of strings describing behaviors we track
       attr_accessor :usage
 
       def initialize(request_id, request_duration_ms, usage: [])

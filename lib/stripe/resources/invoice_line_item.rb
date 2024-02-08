f2f8f842ceac06a566686aeb469712056ebc223e
K0H205 <hrswkhd@gmail.com>
2024-02-08 20:15:55 +0900

    Update InvoiceLineItem class to include a new method for updating line items

--- lib/stripe/resources/invoice_line_item.rb
+++ lib/stripe/resources/invoice_line_item.rb
@@ -2,7 +2,16 @@
 # frozen_string_literal: true
 
 module Stripe
-  class InvoiceLineItem < StripeObject
+  class InvoiceLineItem < APIResource
     OBJECT_NAME = "line_item"
+
+    def update_line_item(invoice, invoice_line_item, params = {}, opts = {})
+      request_stripe_object(
+        method: :post,
+        path: format("/v1/invoices/%<invoice>s/lines/%<invoice_line_item>s", { invoice: CGI.escape(invoice), invoice_line_item: CGI.escape(invoice_line_item) }),
+        params: params,
+        opts: opts
+      )
+    end
   end
 end

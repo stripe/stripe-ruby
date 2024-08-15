05f218433f42b52a51eae3e98e68b93bd88c5597
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-08-15 22:53:45 +0000

    Update generated code for v1202

--- lib/stripe/object_types.rb
+++ lib/stripe/object_types.rb
@@ -72,6 +72,7 @@ module Stripe
         Issuing::Card.object_name => Issuing::Card,
         Issuing::Cardholder.object_name => Issuing::Cardholder,
         Issuing::Dispute.object_name => Issuing::Dispute,
+        Issuing::DisputeSettlementDetail.object_name => Issuing::DisputeSettlementDetail,
         Issuing::PersonalizationDesign.object_name => Issuing::PersonalizationDesign,
         Issuing::PhysicalBundle.object_name => Issuing::PhysicalBundle,
         Issuing::Token.object_name => Issuing::Token,

e41ebb738ac7d5e96cbb6a19598070446a72d623
Jesse Rosalia <jar@stripe.com>
2024-09-25 12:04:10 -0700

    added examples and a new example template

---
+++ examples/README.md
@@ -0,0 +1,11 @@
+## Running an example
+
+From the examples folder, run:
+`RUBYLIB=../lib ruby your_example.rb`
+
+## Adding a new example
+
+1. Clone new_example.rb
+2. Implement your example
+3. Run it (as per above)
+4. 👍
---
+++ examples/new_example.rb
@@ -0,0 +1,23 @@
+require 'stripe'
+require 'date'
+
+class NewExample
+  attr_accessor :api_key
+
+  def initialize(api_key)
+    @api_key = api_key
+  end
+
+  def do_something_great()
+    puts "Hello World"
+    # client = Stripe::StripeClient.new(api_key)
+    # client.v1
+  end
+end
+
+# Send meter events
+api_key = "{{API_KEY}}"
+customer_id = "{{CUSTOMER_ID}}"
+
+example = NewExample.new(api_key)
+example.do_something_great

a49ac73e8352ab48c5bb6f34588cd07f77d5faae
Helen Ye <helenye@stripe.com>
2024-10-01 11:27:46 -0400

    Add necessary fields to thin_event

--- lib/stripe/thin_event.rb
+++ lib/stripe/thin_event.rb
@@ -9,7 +9,9 @@ module Stripe
       @type = event_payload[:type]
       @created = event_payload[:created]
       @context = event_payload[:context]
+      @livemode = event_payload[:livemode]
       @related_object = event_payload[:related_object]
+      @reason = event_payload[:reason]
     end
   end
 end

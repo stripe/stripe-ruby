abb7f96f7e966761062b3f4e50a422a13a453ef1
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-10-07 22:11:40 +0000

    Update generated code for v1273

--- lib/stripe/resources/usage_record_summary.rb
+++ lib/stripe/resources/usage_record_summary.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 module Stripe
+  # A usage record summary represents an aggregated view of how much usage was accrued for a subscription item within a subscription billing period.
   class UsageRecordSummary < StripeObject
     OBJECT_NAME = "usage_record_summary"
     def self.object_name

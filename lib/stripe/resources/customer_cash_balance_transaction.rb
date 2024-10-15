abe64d9057db15747b97e421e05136c5240fd71b
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2024-10-15 01:22:38 +0000

    Update generated code for v1295

--- lib/stripe/resources/customer_cash_balance_transaction.rb
+++ lib/stripe/resources/customer_cash_balance_transaction.rb
@@ -11,5 +11,26 @@ module Stripe
     def self.object_name
       "customer_cash_balance_transaction"
     end
+
+    def test_helpers
+      TestHelpers.new(self)
+    end
+
+    class TestHelpers < APIResourceTestHelpers
+      RESOURCE_CLASS = CustomerCashBalanceTransaction
+      def self.resource_class
+        "CustomerCashBalanceTransaction"
+      end
+
+      # Simulate various customer cash balance side-effects by creating synthetic cash balance transactions in testmode.
+      def self.create(params = {}, opts = {})
+        request_stripe_object(
+          method: :post,
+          path: "/v1/test_helpers/customer_cash_balance_transactions",
+          params: params,
+          opts: opts
+        )
+      end
+    end
   end
 end

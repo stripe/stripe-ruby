95f4c9d231f9a9db0e5fed2058f9d20859adf86d
David Brownman <109395161+xavdid-stripe@users.noreply.github.com>
2024-10-18 11:16:58 -0700

    update object tags for meter-related classes (#1471)
    * update object tags for meter-related classes
    
    * fix tests

--- lib/stripe/resources/v2/billing/meter_event_adjustment.rb
+++ lib/stripe/resources/v2/billing/meter_event_adjustment.rb
@@ -5,9 +5,9 @@ module Stripe
   module V2
     module Billing
       class MeterEventAdjustment < APIResource
-        OBJECT_NAME = "billing.meter_event_adjustment"
+        OBJECT_NAME = "v2.billing.meter_event_adjustment"
         def self.object_name
-          "billing.meter_event_adjustment"
+          "v2.billing.meter_event_adjustment"
         end
       end
     end

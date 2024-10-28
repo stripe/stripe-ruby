fb11d832487a1aff606f243dae4b7adec9657607
helenye-stripe <111009531+helenye-stripe@users.noreply.github.com>
2024-10-28 10:29:13 -0700

    Fix APIResource#retrieve bug not returning instance of custom resources (#1476)
    * Fix refresh bug not returning specific instance type for custom resources
    
    * tests

--- lib/stripe/api_resource.rb
+++ lib/stripe/api_resource.rb
@@ -98,7 +98,14 @@ module Stripe
               "It is not possible to refresh v2 objects. Please retrieve the object using the StripeClient instead."
       end
 
-      @requestor.execute_request_initialize_from(:get, resource_url, :api, self, params: @retrieve_params)
+      @obj = @requestor.execute_request_initialize_from(:get, resource_url, :api, self, params: @retrieve_params)
+      initialize_from(
+        @obj.last_response.data,
+        @obj.instance_variable_get(:@opts),
+        @obj.last_response,
+        api_mode: :v1,
+        requestor: @requestor
+      )
     end
 
     def self.retrieve(id, opts = {})

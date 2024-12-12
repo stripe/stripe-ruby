8ba1ae780091999127c4cab2484c2d3db3410f80
Helen Ye <helenye@stripe.com>
2024-12-12 17:09:57 -0500

    fixes

--- test/stripe/request_params_test.rb
+++ test/stripe/request_params_test.rb
@@ -5,17 +5,20 @@ require File.expand_path("../test_helper", __dir__)
 module Stripe
   class RequestOptionsTest < Test::Unit::TestCase
     class FooCreateParams < Stripe::RequestParams
-      attr_accessor :fun
-      attr_accessor :times
+      attr_accessor :fun, :team
+
+      class Fun < Stripe::RequestParams
+        attr_accessor :games
+      end
     end
 
     context "#to_h" do
       should "convert to hash" do
-
-        params = FooCreateParams.new
-        params.fun = "yes"
-        params.times = 42
-        expected = {fun: "yes", times: 42}
+        params = FooCreateParams.new(
+          fun: FooCreateParams::Fun.new(games: %w[chess go]),
+          team: "blue"
+        )
+        expected = { fun: { games: %w[chess go] }, team: "blue" }
         assert_equal expected, params.to_h
       end
     end

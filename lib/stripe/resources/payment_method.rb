fa51bff2efa06432196d84c03a83a2d3415d0107
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-01-24 06:58:17 +0000

    Update generated code for v1462

--- lib/stripe/resources/payment_method.rb
+++ lib/stripe/resources/payment_method.rb
@@ -200,7 +200,7 @@ module Stripe
       end
 
       class Networks < Stripe::StripeObject
-        # All available networks for the card.
+        # All networks available for selection via [payment_method_options.card.network](/api/payment_intents/confirm#confirm_payment_intent-payment_method_options-card-network).
         attr_reader :available
         # The preferred network for co-branded cards. Can be `cartes_bancaires`, `mastercard`, `visa` or `invalid_preference` if requested network is not valid for the card.
         attr_reader :preferred
@@ -357,7 +357,7 @@ module Stripe
 
     class CardPresent < Stripe::StripeObject
       class Networks < Stripe::StripeObject
-        # All available networks for the card.
+        # All networks available for selection via [payment_method_options.card.network](/api/payment_intents/confirm#confirm_payment_intent-payment_method_options-card-network).
         attr_reader :available
         # The preferred network for the card.
         attr_reader :preferred
@@ -457,7 +457,7 @@ module Stripe
 
     class InteracPresent < Stripe::StripeObject
       class Networks < Stripe::StripeObject
-        # All available networks for the card.
+        # All networks available for selection via [payment_method_options.card.network](/api/payment_intents/confirm#confirm_payment_intent-payment_method_options-card-network).
         attr_reader :available
         # The preferred network for the card.
         attr_reader :preferred

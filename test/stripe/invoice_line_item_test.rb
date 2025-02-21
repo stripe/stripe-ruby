69557bc43d7141f06e6b8f34d9abf3577b35933e
Helen Ye <helenye@stripe.com>
2025-02-21 11:25:13 -0500

    Fix InvoiceLineItem parent class

--- test/stripe/invoice_line_item_test.rb
+++ test/stripe/invoice_line_item_test.rb
@@ -4,5 +4,10 @@ require File.expand_path("../test_helper", __dir__)
 
 module Stripe
   class InvoiceLineItemTest < Test::Unit::TestCase
+    should "be updateable" do
+      item = Stripe::InvoiceLineItem.update("in_123", "il_tmp_123")
+      assert_requested :post, "#{Stripe.api_base}/v1/invoices/in_123/lines/il_tmp_123"
+      assert item.is_a?(Stripe::InvoiceLineItem)
+    end
   end
 end

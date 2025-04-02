6ff0ad06b46d9889e80a35d3dd60d3be69ff1d00
Ramya Rao <100975018+ramya-stripe@users.noreply.github.com>
2025-04-02 09:54:46 -0700

    Fix event names and related objects (#1565)

--- lib/stripe/events/v2_core_account_including_configuration_recipient_capability_status_updated_event.rb
+++ lib/stripe/events/v2_core_account_including_configuration_recipient_capability_status_updated_event.rb
@@ -9,5 +9,15 @@ module Stripe
     end
     # There is additional data present for this event, accessible with the `data` property.
     # See the Stripe API docs for more information.
+
+    # Retrieves the related object from the API. Make an API request on every call.
+    def fetch_related_object
+      _request(
+        method: :get,
+        path: related_object.url,
+        base_address: :api,
+        opts: { stripe_account: context }
+      )
+    end
   end
 end
--- lib/stripe/events/v2_core_account_including_requirements_updated_event.rb
+++ lib/stripe/events/v2_core_account_including_requirements_updated_event.rb
@@ -7,5 +7,15 @@ module Stripe
     def self.lookup_type
       "v2.core.account[requirements].updated"
     end
+
+    # Retrieves the related object from the API. Make an API request on every call.
+    def fetch_related_object
+      _request(
+        method: :get,
+        path: related_object.url,
+        base_address: :api,
+        opts: { stripe_account: context }
+      )
+    end
   end
 end

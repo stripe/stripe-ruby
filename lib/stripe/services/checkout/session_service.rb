73a1d2da5375eeb23cff1f47f5d7ac0cfce195e5
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-07-14 08:08:25 +0000

    Update generated code for v1836 and 2025-06-30.preview

--- lib/stripe/services/checkout/session_service.rb
+++ lib/stripe/services/checkout/session_service.rb
@@ -1486,9 +1486,18 @@ module Stripe
           class Pix < Stripe::RequestParams
             # The number of seconds (between 10 and 1209600) after which Pix payment will expire. Defaults to 86400 seconds.
             attr_accessor :expires_after_seconds
+            # Indicates that you intend to make future payments with this PaymentIntent's payment method.
+            #
+            # If you provide a Customer with the PaymentIntent, you can use this parameter to [attach the payment method](/payments/save-during-payment) to the Customer after the PaymentIntent is confirmed and the customer completes any required actions. If you don't provide a Customer, you can still [attach](/api/payment_methods/attach) the payment method to a Customer after the transaction completes.
+            #
+            # If the payment method is `card_present` and isn't a digital wallet, Stripe creates and attaches a [generated_card](/api/charges/object#charge_object-payment_method_details-card_present-generated_card) payment method representing the card to the Customer instead.
+            #
+            # When processing card payments, Stripe uses `setup_future_usage` to help you comply with regional legislation and network rules, such as [SCA](/strong-customer-authentication).
+            attr_accessor :setup_future_usage
 
-            def initialize(expires_after_seconds: nil)
+            def initialize(expires_after_seconds: nil, setup_future_usage: nil)
               @expires_after_seconds = expires_after_seconds
+              @setup_future_usage = setup_future_usage
             end
           end
 

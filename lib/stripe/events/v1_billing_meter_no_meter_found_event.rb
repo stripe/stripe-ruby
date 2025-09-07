c4b56112ba17aec6e43c0067870500f90e98b69e
David Brownman <xavdid@stripe.com>
2025-09-06 19:57:24 -0700

    generate event data types

--- lib/stripe/events/v1_billing_meter_no_meter_found_event.rb
+++ lib/stripe/events/v1_billing_meter_no_meter_found_event.rb
@@ -7,8 +7,85 @@ module Stripe
     def self.lookup_type
       "v1.billing.meter.no_meter_found"
     end
-    # There is additional data present for this event, accessible with the `data` property.
-    # See the Stripe API docs for more information.
+
+    class V1BillingMeterNoMeterFoundEventData < Stripe::StripeObject
+      class Reason < Stripe::StripeObject
+        class ErrorType < Stripe::StripeObject
+          class SampleError < Stripe::StripeObject
+            class Request < Stripe::StripeObject
+              # The request idempotency key.
+              attr_reader :identifier
+
+              def self.inner_class_types
+                @inner_class_types = {}
+              end
+
+              def self.field_remappings
+                @field_remappings = {}
+              end
+            end
+            # The error message.
+            attr_reader :error_message
+            # The request causes the error.
+            attr_reader :request
+
+            def self.inner_class_types
+              @inner_class_types = { request: Request }
+            end
+
+            def self.field_remappings
+              @field_remappings = {}
+            end
+          end
+          # Open Enum.
+          attr_reader :code
+          # The number of errors of this type.
+          attr_reader :error_count
+          # A list of sample errors of this type.
+          attr_reader :sample_errors
+
+          def self.inner_class_types
+            @inner_class_types = { sample_errors: SampleError }
+          end
+
+          def self.field_remappings
+            @field_remappings = {}
+          end
+        end
+        # The total error count within this window.
+        attr_reader :error_count
+        # The error details.
+        attr_reader :error_types
+
+        def self.inner_class_types
+          @inner_class_types = { error_types: ErrorType }
+        end
+
+        def self.field_remappings
+          @field_remappings = {}
+        end
+      end
+      # Extra field included in the event's `data` when fetched from /v2/events.
+      attr_reader :developer_message_summary
+      # This contains information about why meter error happens.
+      attr_reader :reason
+      # The end of the window that is encapsulated by this summary.
+      attr_reader :validation_end
+      # The start of the window that is encapsulated by this summary.
+      attr_reader :validation_start
+
+      def self.inner_class_types
+        @inner_class_types = { reason: Reason }
+      end
+
+      def self.field_remappings
+        @field_remappings = {}
+      end
+    end
+
+    def self.inner_class_types
+      @inner_class_types = { data: V1BillingMeterNoMeterFoundEventData }
+    end
   end
 
   # Occurs when a Meter's id is missing or invalid in async usage events.

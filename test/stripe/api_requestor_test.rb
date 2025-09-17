77b83c13640f87e5c0b30ce4f20c89e839f2e852
David Brownman <109395161+xavdid-stripe@users.noreply.github.com>
2025-09-17 13:16:54 -0700

    re-add DeletedObject code to private-preview (#1655)
    * generate w/ mid-sept spec
    
    * comment out v2 deletedobject things
    
    * remove v2 deleted references
    
    * generate w/ mid-sept spec
    
    * Revert "remove v2 deleted references"
    
    This reverts commit c1a92cf14d751676ade5585fcc02bb79cd2f8b34.
    
    * Revert "comment out v2 deletedobject things"
    
    This reverts commit be63499d5367150aed49dc495116c28c7e54ae0d.
    
    * Revert "comment out v2 deletedobject things"
    
    This reverts commit be63499d5367150aed49dc495116c28c7e54ae0d.
    
    * Revert "remove v2 deleted references"
    
    This reverts commit c1a92cf14d751676ade5585fcc02bb79cd2f8b34.
    
    * fix deleted objects

--- test/stripe/api_requestor_test.rb
+++ test/stripe/api_requestor_test.rb
@@ -1296,22 +1296,22 @@ module Stripe
         )
       end
 
-      #   %w[v1 v2].each do |mode|
-      #     should "correctly handle #{mode} delete request responses" do
-      #       stub_request(:delete, "#{Stripe::DEFAULT_API_BASE}/#{mode}/foo")
-      #         .to_return(body: JSON.generate(object: "customer", id: "abc_123"), status: 200)
+      %w[v1 v2].each do |mode|
+        should "correctly handle #{mode} delete request responses" do
+          stub_request(:delete, "#{Stripe::DEFAULT_API_BASE}/#{mode}/foo")
+            .to_return(body: JSON.generate(object: "customer", id: "abc_123"), status: 200)
 
-      #       requestor = APIRequestor.new("sk_test_123")
+          requestor = APIRequestor.new("sk_test_123")
 
-      #       response = requestor.execute_request(:delete, "/#{mode}/foo", :api)
+          response = requestor.execute_request(:delete, "/#{mode}/foo", :api)
 
-      #       if mode == "v1"
-      #         assert_instance_of Stripe::Customer, response
-      #       else
-      #         assert_instance_of Stripe::V2::DeletedObject, response
-      #       end
-      #     end
-      #   end
+          if mode == "v1"
+            assert_instance_of Stripe::Customer, response
+          else
+            assert_instance_of Stripe::V2::DeletedObject, response
+          end
+        end
+      end
     end
 
     context "#execute_request_stream" do

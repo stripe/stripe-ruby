10cffcf9439122bb0bb2d8e6c0f67bbf7eafa95a
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-10-06 15:12:34 +0000

    Update generated code for v2041 and

--- lib/stripe/resources/charge.rb
+++ lib/stripe/resources/charge.rb
@@ -1786,6 +1786,9 @@ module Stripe
       end
 
       class Rechnung < Stripe::StripeObject
+        # Payment portal URL.
+        attr_reader :payment_portal_url
+
         def self.inner_class_types
           @inner_class_types = {}
         end

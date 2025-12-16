3333c9336b08b6b730e8ecee2fa05aabd82a0026
David Brownman <xavdid@stripe.com>
2025-12-16 14:56:44 -0800

    linting

--- examples/event_notification_handler_endpoint.rb
+++ examples/event_notification_handler_endpoint.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 # typed: false
 
-# event_notification_handler_endpoint.rb - receive and process event notifications (AKA thin events) like "v1.billing.meter.error_report_triggered" using EventNotificationHandler.
+# event_notification_handler_endpoint.rb - receive and process event notifications (AKA thin events) like
+# "v1.billing.meter.error_report_triggered" using EventNotificationHandler.
 
 # In this example, we:
 #     - write a fallback callback to handle unrecognized event notifications
@@ -19,11 +20,11 @@ webhook_secret = ENV.fetch("WEBHOOK_SECRET", nil)
 
 client = Stripe::StripeClient.new(api_key)
 
-handler = client.notification_handler(webhook_secret) do |notif, client, details|
+handler = client.notification_handler(webhook_secret) do |notif, _client, _details|
   puts "Received unhandled notification:", notif.type
 end
 
-handler.on_v1_billing_meter_error_report_triggered do |event_notification, client|
+handler.on_v1_billing_meter_error_report_triggered do |event_notification, _client|
   meter = event_notification.fetch_related_object
   puts "Meter #{meter.display_name} (#{meter.id}) had a problem"
 end

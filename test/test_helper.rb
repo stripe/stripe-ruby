6820ec48d4adec25f1272095eaf302ea4f2f79a1
David Brownman <xavdid@stripe.com>
2026-07-15 15:43:23 -0700

    maybe fix tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,21 +14,23 @@ require File.expand_path("stripe_mock", __dir__)
 
 MOCK_MINIMUM_VERSION = "0.109.0"
 MOCK_PORT = Stripe::StripeMock.start
+# to handle the override we need for windows
+MOCK_HOST = ENV.fetch("STRIPE_MOCK_HOST", "localhost")
 
 # Disable all real network connections except those that are outgoing to
 # stripe-mock.
-WebMock.disable_net_connect!(allow: "localhost:#{MOCK_PORT}")
+WebMock.disable_net_connect!(allow: "#{MOCK_HOST}:#{MOCK_PORT}")
 
 # Try one initial test connection to stripe-mock so that if there's a problem
 # we can print one error and fail fast so that it's more clear to the user how
 # they should fix the problem.
 begin
-  resp = Net::HTTP.get_response(URI("http://localhost:#{MOCK_PORT}/"))
+  resp = Net::HTTP.get_response(URI("http://#{MOCK_HOST}:#{MOCK_PORT}/"))
   version = resp["Stripe-Mock-Version"]
 
   if version.nil?
     abort("Couldn't find `Stripe-Mock-Version` in response from " \
-          "`localhost:#{MOCK_PORT}`. Is the service running there stripe-mock?")
+          "`#{MOCK_HOST}:#{MOCK_PORT}`. Is the service running there stripe-mock?")
   end
 
   if version != "master" &&
@@ -38,7 +40,7 @@ begin
           "see its repository for upgrade instructions.")
   end
 rescue Errno::ECONNREFUSED
-  abort("Couldn't reach stripe-mock at `localhost:#{MOCK_PORT}`. Is " \
+  abort("Couldn't reach stripe-mock at `#{MOCK_HOST}:#{MOCK_PORT}`. Is " \
         "it running? Please see README for setup instructions.")
 end
 
@@ -56,8 +58,8 @@ module Test
 
       setup do
         Stripe.api_key = "sk_test_123"
-        Stripe.api_base = "http://localhost:#{MOCK_PORT}"
-        Stripe.uploads_base = "http://localhost:#{MOCK_PORT}"
+        Stripe.api_base = "http://#{MOCK_HOST}:#{MOCK_PORT}"
+        Stripe.uploads_base = "http://#{MOCK_HOST}:#{MOCK_PORT}"
 
         stub_connect
       end
